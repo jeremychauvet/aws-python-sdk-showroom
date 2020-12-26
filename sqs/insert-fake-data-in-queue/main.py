4a8b6b098b53675bd99f2509178972dc30cac6ee
Jérémy Chauvet <jer.chauvet@gmail.com>
2020-12-26 23:21:01 +0100

    Improve error catching.

--- sqs/insert-fake-data-in-queue/main.py
+++ sqs/insert-fake-data-in-queue/main.py
@@ -13,27 +13,53 @@ client = boto3.client("sqs", region_name="eu-central-1")
 
 # Variables.
 QUEUE_NAME = "order.fifo"
+NUMBER_OF_MESSAGES_TO_SEND = 10
 
 
 def insert_messages_in_queue():
-    get_queue_url_response = client.get_queue_url(QueueName=QUEUE_NAME)
-    queue_url = get_queue_url_response["QueueUrl"]
+    try:
+        get_queue_url_response = client.get_queue_url(QueueName=QUEUE_NAME)
+        queue_url = get_queue_url_response["QueueUrl"]
 
-    fake = Faker()
-    fake.add_provider(CreditScore)
+        fake = Faker()
+        fake.add_provider(CreditScore)
 
-    message = str(fake.credit_score_name()) + " - " + str(fake.credit_score_provider()) + \
-        " - " + str(fake.credit_score())
-    hash = secrets.token_hex(nbytes=16)
-    send_message_response = client.send_message(
-        QueueUrl=queue_url, MessageBody=message, MessageGroupId=hash, MessageDeduplicationId=hash)
+        for message_number in range(NUMBER_OF_MESSAGES_TO_SEND):
+            message = (
+                str(fake.credit_score_name())
+                + " - "
+                + str(fake.credit_score_provider())
+                + " - "
+                + str(fake.credit_score())
+            )
+            hash = secrets.token_hex(nbytes=16)
+            print('[INFO] Message : ' + message)
+            print('[INFO] Hash : ' + hash)
+            send_message_response = client.send_message(
+                QueueUrl=queue_url,
+                MessageBody=message,
+                MessageGroupId=hash,
+                MessageDeduplicationId=hash,
+            )
 
-    if send_message_response['ResponseMetadata']['HTTPStatusCode'] == 200:
-        print("[INFO] Message sent successfully. Output : " + str(send_message_response))
-    else:
-        print("[ERROR] Message not sent successfully. Output : " +
-              str(send_message_response))
+            if send_message_response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+                print(
+                    "[INFO] Message "
+                    + str(message_number)
+                    + " of "
+                    + str(NUMBER_OF_MESSAGES_TO_SEND)
+                    + " sent successfully."
+                )
+                sleep(2)
+            else:
+                print(
+                    "[ERROR] Message not sent successfully. Output : "
+                    + str(send_message_response)
+                )
+                sys.exit(1)
 
+    except:
+        print("[ERROR] " + str(sys.exc_info()[1]))
 
 if __name__ == "__main__":
     insert_messages_in_queue()
